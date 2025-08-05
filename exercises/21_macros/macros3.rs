ca1a24445cbcd629ee2a6b3f54f056e8deb5ef78
r_mayyank <thelueblue@gmail.com>
2025-08-05 19:47:00 +0530

    macro q3,4

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,13 +1,14 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
-            println!("Check out my macro!");
+            println!("Check out my macro!")
         };
     }
 }
 
 fn main() {
-    my_macro!();
+    my_macro!()
 }
